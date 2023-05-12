57c99b05485ff3d639bf9623a485c78fce240737
maha <maha@gobis-air.home>
2023-05-12 00:27:03 -0400

    added test for Header,Headercart,CartIcon and MainImage component

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import Header from './Header'
+import CartContext from '../../store/cart-context';
+
+
+describe("Header Component", () => {
+	it('loads and displays "YummyTreat"', async () => {
+		// ARRANGE
+		render(<Header />)
+
+		// ASSERT
+		expect(screen.getByRole('heading')).toHaveTextContent('YummyTreat')
+	})
+
+	// TODO test render HeaderCartButton
+	it("renders <HeaderCartButton/> ", () => {
+		const { queryByTestId } = render(<Header />);
+		const CartButton = queryByTestId('cart-button')
+		expect(CartButton).toBeInTheDocument()
+	})
+
+	// TODO test render MainImage
+
+	it("renders <MainImage /> ", () => {
+		const { queryByTestId } = render(<Header />);
+		const img = queryByTestId('main-img')
+		expect(img).toBeInTheDocument()
+	})
+
+
+});
--- src/components/Header/MainImage.js
+++ src/components/Header/MainImage.js
@@ -4,10 +4,9 @@ import classes from "./MainImage.module.css";
 
 const MainImage = () => {
   return (
-    <div className={classes["main-image"]}>
+    <div data-testid='main-img' className={classes["main-image"]}>
       <img src={mealsImage} alt="A Table Full Of Delicious Food!." />
     </div>
   );
 };
-
 export default MainImage;
