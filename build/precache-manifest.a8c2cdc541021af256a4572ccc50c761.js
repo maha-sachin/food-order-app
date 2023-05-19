b5001d47677dd31ac842a344548025da2ed0147b
Pachari Sachin <sachin@Pacharis-Laptop.ht.home>
2023-05-19 17:20:52 -0400

    built using react-scripts, deploy using firebase

---
+++ build/precache-manifest.a8c2cdc541021af256a4572ccc50c761.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e80639ba0abe1c28aea",
+    "url": "/static/css/main.703d3a92.chunk.css"
+  },
+  {
+    "revision": "6e80639ba0abe1c28aea",
+    "url": "/static/js/main.6e80639b.chunk.js"
+  },
+  {
+    "revision": "8aa05ee9adcca2ea18c1",
+    "url": "/static/css/1.034238d8.chunk.css"
+  },
+  {
+    "revision": "8aa05ee9adcca2ea18c1",
+    "url": "/static/js/1.8aa05ee9.chunk.js"
+  },
+  {
+    "revision": "dc42e89534d7d02218bd",
+    "url": "/static/js/2.dc42e895.chunk.js"
+  },
+  {
+    "revision": "932fadd2557755b75c1a",
+    "url": "/static/js/runtime~main.932fadd2.js"
+  },
+  {
+    "revision": "55c749900b91566d7fa1eea3937e0e04",
+    "url": "/static/media/meals.55c74990.jpeg"
+  },
+  {
+    "revision": "122a540a1b68db315423dbafff2d61f7",
+    "url": "/index.html"
+  }
+];
